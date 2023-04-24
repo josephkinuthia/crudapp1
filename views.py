a20f45266d8b964ab3579bc0ef2a8334bf9ef263
josephkinuthia <128019685+josephkinuthia@users.noreply.github.com>
2023-04-24 01:34:39 -0400

    Add files via upload

---
+++ views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from crudapp.models import Student
+from crudapp.forms import studentForm
+
+
+
+# Create your views here.
+class StudentForm:
+    pass
+
+
+def index(request):
+    if request.method == 'POST':
+        form = studentForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+
+    else:
+        form = studentForm()
+        return render(request, 'index.html', {'form': form})
+
+
+def show(request):
+    students = Student.objects.all()
+    return render(request, 'show.html', {'students': students})
+
+
+
+def edit(request,id):
+    student = Student.objects.get(id=id)
+    return render(request, 'edit.html', {'student': student})
+
+
+def delete(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('/show')
+
+
+
+
+def update(request, id):
+    student = Student.objects.get(id=id)
+    form = studentForm(request.POST, instance=student)
+    if form.is_valid():
+            form.save()
+            return redirect('/show')
+    else:
+        return render(request, 'edit.html', {'student': student})
+
+
